8bfa8185baaca408a07ad66aa66a305382dd72bd
David Pires <developer.david@outlook.com>
2022-02-13 21:19:12 -0300

    Convention of the use of (props) arguments and atributes

--- pages/basicos/utilizingTitle.jsx
+++ pages/basicos/utilizingTitle.jsx
@@ -2,7 +2,15 @@ import Titulo from '../../components/Titulo'
 export default function utilizingTitle() {
   return (    
       <div>
-        <Titulo/>               
+        <Titulo 
+                principal="Página de Cadastro"
+                secundario="Incluir, alterar e excluir coisas!"
+        />   
+        <Titulo 
+        principal="Página de Login"
+        secundario="Informe seu e-mail e senha"
+        pequeno={true}
+/>            
       </div>    
   );
 }
