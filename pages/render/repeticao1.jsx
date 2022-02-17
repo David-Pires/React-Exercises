33c200fb29e9ee0946d392eab015c211eb12f885
David Pires <developer.david@outlook.com>
2022-02-17 17:32:22 -0300

    repetição

---
+++ pages/render/repeticao1.jsx
@@ -0,0 +1,26 @@
+export default function Repeticao1() {
+  const listaAprovados = [
+    'João',
+    'Maria',
+    'Pedro',
+    'Lucio',
+    'Cheguevara',
+    'Marcos',
+    'Cristovão',
+  ]
+
+  function renderizarLista() {
+    return (
+      <>
+      <li>Elemento #1</li>
+      <li>Elemento #2</li>
+      <li>Elemento #3</li>
+      
+      </>
+    )
+  }
+  return (
+    <ul>
+    </ul>
+  )
+}
