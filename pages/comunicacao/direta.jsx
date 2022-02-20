6ec48483c6b372967f11a49dae14e9acef1f57de
David Pires <developer.david@outlook.com>
2022-02-19 22:02:50 -0300

    direct communication

---
+++ pages/comunicacao/direta.jsx
@@ -0,0 +1,10 @@
+import Filho from "../../components/direta/Filho";
+import Pai from "../../components/direta/Pai";
+
+export default function Direta() {
+  return (
+    <div>
+      <Pai familia="Albuquerque" nome="Padrão"/>
+    </div>
+  );
+}
