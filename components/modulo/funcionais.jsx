25bb5e395af22ff54e624357ee528bdaaa66d798
David Pires <developer.david@outlook.com>
2022-02-22 18:36:17 -0300

    functionais

---
+++ components/modulo/funcionais.jsx
@@ -0,0 +1,26 @@
+export function Comp1() {
+  return ( 
+    <h2>Comp1</h2>
+  ) 
+}
+
+const Comp2 = function () {
+  return <h2>Comp2</h2>
+}
+
+
+
+
+
+
+
+
+// export const Comp2 = function() {
+//   return <h2>Comp2</h2>
+// }
+
+
+
+// export default 
+
+// npm install --save-dev typescript @types/react @types/node
